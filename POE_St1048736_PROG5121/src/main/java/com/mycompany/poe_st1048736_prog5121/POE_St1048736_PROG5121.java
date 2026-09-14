92d967a622923a33e6bac85eba8329c9ab3bd1af
St10480736 <st10480736@vcconnect.edu.za>
2026-09-14 15:09:58 +0200

    Initialized Project

---
+++ POE_St1048736_PROG5121/src/main/java/com/mycompany/poe_st1048736_prog5121/POE_St1048736_PROG5121.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poe_st1048736_prog5121;
+
+/**
+ *
+ * @author Kganya
+ */
+public class POE_St1048736_PROG5121 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
